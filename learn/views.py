c0abe0227e2f60dddccfffda1467dbfcf8832b8b
cahesi <agt1234@foxmail.com>
2016-11-03 12:31:50 +0800

    rm learn

--- learn/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# encoding : utf-8
-from django.http import HttpResponse
- 
-def index(request):
-    # return HttpResponse('hello world !')
-    return render(request , 'home.html')
